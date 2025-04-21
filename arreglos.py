07a94c6aff08bdd4846a12a3e96b3d016cb35cd5
elgatomaleante12 <12joaquinalvarado12@gmail.com>
2025-04-21 13:30:17 -0400

    avances de la clase de arreglos

---
+++ arreglos.py
@@ -0,0 +1,16 @@
+#hola soy un comentario 
+nombre = "joaco" 
+edad = 30 
+estatura =1.6
+esDocente = True
+frutas = ["manzana", "pera", "pera", "frutilla"]
+print(frutas[2])
+
+
+#entonces, como se hace??
+estudiante = [
+    {"nombre": "maxi", "edad": 20, "curso": "2A"},
+    {"nombre": "richard", "edad": 20, "curso": "4A"}
+]
+
+print(estudiante)
